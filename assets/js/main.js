7a73f29de541b891a3bbde23ff6c649f2343e905
Ezequiel Quintana <equintana779@gmail.com>
2024-11-22 16:22:47 -0300

    correcion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Day
-* Template URL: https://bootstrapmade.com/day-multipurpose-html-template-for-free/
-* Updated: Jun 02 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 // Selecciona el botón y el menú
 const dropdownToggle = document.querySelector('.dropdown-toggle');
 const dropdownMenu = document.getElementById('flags');
